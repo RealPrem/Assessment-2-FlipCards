110cf3a4437064fcd8978b21c2c7de9775fd2ad4
prem@ascot.ac.th <prem@ascot.ac.th>
2021-03-15 12:14:34 +0700

    Add project files.

---
+++ Assessment 2 FlipCards/Card.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment_2_FlipCards
+{
+    class Card
+    {
+        private string Question;
+        private string Answer;
+        private bool Flipped;
+
+        public Card(string Question, string Answer)
+        {
+            this.Question = Question;
+            this.Answer = Answer;
+            Flipped = false;
+        }
+        public bool IsFlipped()
+        {
+            return Flipped;
+        }
+        public void Flip()
+        {
+            if (!Flipped)
+            {
+                Flipped = true;
+            }
+            else
+            {
+                Flipped = false;
+            }
+        }
+        public string GetQuestion()
+        {
+            return Question;
+        }
+        public string GetAnswer()
+        {
+            if (!Flipped)
+            {
+                return ("NO CHEATING");
+            }
+            return Answer;
+        }
+
+
+    }
+}
